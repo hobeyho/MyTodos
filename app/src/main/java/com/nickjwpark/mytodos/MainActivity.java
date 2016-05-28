044bf49ac477dac1e532cae4a192011e84c54931
Nick JongWook Park <Nick@Nicks-MacBook-Pro.local>
2016-05-28 15:05:55 +0900

    Initial commit

---
+++ app/src/main/java/com/nickjwpark/mytodos/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nickjwpark.mytodos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//MyTodo list with id/password protection
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
