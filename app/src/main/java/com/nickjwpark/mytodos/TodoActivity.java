c06178b53172734c88302152cff2a08d4b26314b
Nick JongWook Park <Nick@Nicks-MacBook-Pro.local>
2016-05-28 16:41:04 +0900

    Layout finished

--- app/src/main/java/com/nickjwpark/mytodos/TodoActivity.java
+++ app/src/main/java/com/nickjwpark/mytodos/TodoActivity.java
@@ -1,12 +1,27 @@
 package com.nickjwpark.mytodos;
 
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class TodoActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class TodoActivity extends ListActivity {
 
     TextView textViewGreeting;
+    Button btnAdd;
+    ArrayList<String> todo_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +29,57 @@ public class TodoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_todo);
 
         textViewGreeting = (TextView) findViewById(R.id.textViewGreeting);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
 
         Bundle extras = getIntent().getExtras();
         String id = "";
         if (extras != null) {
             id = extras.getString("id"); //get extra with key of "id" and set it to variable id
         }
+        textViewGreeting.setText("" + id + "의 할일들");
 
-        textViewGreeting.setText("Hello " + id + "!");
+        todo_list = new ArrayList<String>();
+        todo_list.add("todo1");
+        todo_list.add("todo2");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, todo_list);
+        setListAdapter(adapter);
 
+
+        btnAdd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                addNewTodo();
+            }
+        });
+    }
+
+    //open alert dialog to create a new todo
+    public void addNewTodo(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("추가");
+        builder.setMessage("할 일을 추가해 보세요");
+
+        final EditText todo_input = new EditText(this);
+        todo_input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
+        builder.setView(todo_input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //할일을 여기서 저장하자
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
     }
 }
+
